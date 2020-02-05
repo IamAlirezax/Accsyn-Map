8b1a7e337a0c04d65b2f7e35d83c22188379c0be
Alireza <amohammadi84@yahoo.se>
2020-02-05 13:15:43 +0100

    Add API key

--- accyn-map/src/App.js
+++ accyn-map/src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { GoogleMap , withScriptjs,withGoogleMap } from "google-map-react" ;
+import { GoogleMap , withScriptjs,withGoogleMap } from "react-google-maps" ;
 
 function Map() {
 return (
 <GoogleMap
  default Zoom = {10}
- defaultCenter={{lat : 12, lng : 12  }}
+ defaultCenter= {{lat : 45.421532, lng : -75.697189 }}
     />
    );
  }
@@ -15,11 +15,13 @@ return (
 export default function App( ) {
 
   return( 
-  <div>
-    <WrappedMap googleMapUrl={'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places'} />
-   loadingElement = {<div style={{height: "100%" }} />}
-   containerElement={<div style={{ height: `400px` }} />}
-   mapElement={<div style={{ height: `100%` }} />}
+  <div style= {{width : '100vw' , height: '100vh'}}>
+    <WrappedMap 
+  googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyDMyQXqBsikFoovFFfHM6pAUCaXQZdCPtg"
+  loadingElement={<div style={{ height: `100%` }} />}
+  containerElement={<div style={{ height: `400px` }} />}
+  mapElement={<div style={{ height: `100%` }} />}
+   />
   </div>
   );
 }
