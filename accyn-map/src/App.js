f232891e9a996818bcd892c84c1bf73aa08711c7
Alireza <amohammadi84@yahoo.se>
2020-02-05 09:30:10 +0100

    Uppdate

--- accyn-map/src/App.js
+++ accyn-map/src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { GoogleMap , withScriptjs,withGoogleMap } from "google-map-react" ;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function Map() {
+return (
+<GoogleMap
+ default Zoom = {10}
+ defaultCenter={{lat : 12, lng : 12  }}
+    />
+   );
+ }
+
+ const WrappedMap = withScriptjs(withGoogleMap(Map));
+
+export default function App( ) {
+
+  return( 
+  <div>
+    <WrappedMap googleMapUrl={'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places'} />
+   loadingElement = {<div style={{height: "100%" }} />}
+   containerElement={<div style={{ height: `400px` }} />}
+   mapElement={<div style={{ height: `100%` }} />}
+  </div>
   );
 }
-
-export default App;
