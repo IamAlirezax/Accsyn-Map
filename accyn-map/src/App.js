541053b94ee52a7f4df38a3c281eecd51718bb07
Alireza <amohammadi84@yahoo.se>
2020-02-17 13:40:41 +0100

    Start The Map

--- accyn-map/src/App.js
+++ accyn-map/src/App.js
@@ -1,27 +1,37 @@
 import React from "react";
-import { GoogleMap , withScriptjs,withGoogleMap } from "react-google-maps" ;
+import { GoogleMap , withScriptjs,withGoogleMap, Marker } from "react-google-maps" ;
+import * as ExampleData from "./data/mock.json";
 
 function Map() {
 return (
 <GoogleMap
- default Zoom = {10}
+ defaultZoom = {10}
  defaultCenter= {{lat : 45.421532, lng : -75.697189 }}
-    />
+ >
+   {ExampleData.clients.map((client) => (
+     <Marker key={client.location.ip} position = {{lat : client.location.latitude, 
+      lng : client.location.longitude }}
+      />
+
+    ))}
+ </GoogleMap>
+   
    );
  }
 
- const WrappedMap = withScriptjs(withGoogleMap(Map));
+ const MyMapComponent = withScriptjs(withGoogleMap(Map));
 
 export default function App( ) {
 
   return( 
   <div style= {{width : '100vw' , height: '100vh'}}>
-    <WrappedMap 
-  googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyDMyQXqBsikFoovFFfHM6pAUCaXQZdCPtg"
+   <MyMapComponent
+  isMarkerShown
+  googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyDMyQXqBsikFoovFFfHM6pAUCaXQZdCPtg&libraries=geometry,drawing,places"
   loadingElement={<div style={{ height: `100%` }} />}
   containerElement={<div style={{ height: `400px` }} />}
   mapElement={<div style={{ height: `100%` }} />}
-   />
+/>
   </div>
   );
 }
