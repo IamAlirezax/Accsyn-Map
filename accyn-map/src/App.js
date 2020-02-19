492f232a2f0d7bcbc95ba96a6ad8b2800aeb04b3
Alireza <amohammadi84@yahoo.se>
2020-02-19 14:53:13 +0100

    Add Icon and set details

--- accyn-map/src/App.js
+++ accyn-map/src/App.js
@@ -1,19 +1,46 @@
-import React from "react";
-import { GoogleMap , withScriptjs,withGoogleMap, Marker } from "react-google-maps" ;
+import React, { useState } from "react";
+import { GoogleMap , withScriptjs,withGoogleMap, Marker, InfoWindow } from "react-google-maps" ;
 import * as ExampleData from "./data/mock.json";
 
 function Map() {
+  const [selectedClient, setSelectedClient] = useState(null)
 return (
 <GoogleMap
  defaultZoom = {10}
  defaultCenter= {{lat :17.385044, lng : 78.486671 }}
  >
    {ExampleData.clients.map((client) => (
-     <Marker key={client.code} position = {{lat : client.location.latitude, 
+     <Marker
+
+      key={client.code} 
+      position = {{lat : client.location.latitude, 
       lng : client.location.longitude }}
+
+      onClick={() => {
+      setSelectedClient(client);
+      }}
+      icon = {{
+        url: '/Filmgate_mot_svart_4k_RGB.svg',
+        scaledSize: new window.google.maps.Size(80,80)
+      }}
       />
 
     ))}
+{selectedClient && (
+  <InfoWindow
+  position = {{lat : selectedClient.location.latitude, 
+    lng : selectedClient.location.longitude }}
+    onCloseClick = {() => {setSelectedClient(null)
+
+    }}
+     >
+    <div> Ｃｌｉｅｎｔ Ｄｅｔａｉｌｓ
+      <h2>{selectedClient.location.isp}</h2>
+      <p>{selectedClient.location.city}</p>
+      <p1>{selectedClient.status}</p1>
+    </div>
+  </InfoWindow>
+)}
  </GoogleMap>
    
    );
